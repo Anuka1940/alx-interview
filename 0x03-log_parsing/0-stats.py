ec247ef20d3f7c768b1dcd6b253be1c0ee4b1308
root <root@DESKTOP-J88VO51.localdomain>
2024-10-24 15:09:06 -0700

    rewrite 0-stats

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -3,48 +3,43 @@
 """Script that reads stdin and computes metrics"""
 
 import sys
-import signal
-
-total_size = 0
-status_counts = {200: 0, 301: 0, 400: 0, 401: 0,
-                 403: 0, 404: 0, 405: 0, 500: 0}
-line_count = 0
 
 
-def print_stats():
-    """Prints the accumulated file size and status code counts."""
-    print(f"File size: {total_size}")
-    for code in sorted(status_counts.keys()):
-        if status_counts[code] > 0:
-            print(f"{code}: {status_counts[code]}")
+def printsts(dic, size):
+    """ WWPrints information """
+    print("File size: {:d}".format(size))
+    for i in sorted(dic.keys()):
+        if dic[i] != 0:
+            print("{}: {:d}".format(i, dic[i]))
 
 
-def signal_handler(sig, frame):
-    """Handle keyboard interrupt (Ctrl + C)"""
-    print_stats()
-    sys.exit(0)
+sts = {"200": 0, "301": 0, "400": 0, "401": 0, "403": 0,
+       "404": 0, "405": 0, "500": 0}
+
+count = 0
+size = 0
+
+try:
+    for line in sys.stdin:
+        if count != 0 and count % 10 == 0:
+            printsts(sts, size)
+
+        stlist = line.split()
+        count += 1
+
+        try:
+            size += int(stlist[-1])
+        except:
+            pass
+
+        try:
+            if stlist[-2] in sts:
+                sts[stlist[-2]] += 1
+        except:
+            pass
+    printsts(sts, size)
 
 
-signal.signal(signal.SIGINT, signal_handler)
-
-for line in sys.stdin:
-    try:
-        parts = line.split()
-        if len(parts) < 7:
-            continue
-
-        status_code = int(parts[-2])
-        file_size = int(parts[-1])
-
-        total_size += file_size
-
-        if status_code in status_counts:
-            status_counts[status_code] += 1
-
-        line_count += 1
-
-        if line_count % 10 == 0:
-            print_stats()
-
-    except (ValueError, IndexError):
-        continue
+except KeyboardInterrupt:
+    printsts(sts, size)
+    raise
