c5e13a82990731af1ae96519489c7d99bf8bb747
root <root@DESKTOP-J88VO51.localdomain>
2024-10-31 07:41:03 -0700

    improve code

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -6,8 +6,22 @@
 
 
 def validUTF8(data):
-    if isinstance(data, list):
-        if not all(0 <= byte <= 255 for byte in data):
-            return False
+    remaining_bytes = 0
+
+    for byte in data:
+        byte = byte & 0xFF
+
+        if remaining_bytes == 0:
+            if byte >> 7 == 0:
+                continue
+            elif byte >> 5 == 0b110:
+                remaining_bytes = 1
+            elif byte >> 4 == 0b1110:
+                remaining_bytes = 2
+            elif byte >> 3 == 0b11110:
+                remaining_bytes = 3
         else:
-            return True
+            if byte >> 6 != 0b10:
+                return False
+            remaining_bytes -= 1
+    return remaining_bytes == 0
